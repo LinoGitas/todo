207613187dd34b79d95c23ec485a7fc484bceb23
LinoGitas <63246130+LinoGitas@users.noreply.github.com>
2020-05-11 18:56:26 +0300

    fix todo list

--- js/todo.js
+++ js/todo.js
@@ -1,62 +1,3 @@
-/*
-
-String.prototype.toAlternatingCase = function () {
-    let text = '';
-
-    for (let i=0; i<this.length; i++) {
-        let letters = this[i];
-        
-        
-        if (letters === letters.toLowerCase() )
-        {
-            // jei raide mazoji
-            text = text + letters.toUpperCase();        
-        }
-        else{
-            // jei raide didzioji
-            text = text + letters.toLowerCase();
-        }
-    }
-    //console.log(text);
-    return text;
-
-    
-}
-
-console.log("Linas".toAlternatingCase() === "lINAS");
-
-
-String.prototype.firstLetter = function () {
-    console.log( this );
-    return this[0];
-}
-
-console.log( 'afghfgxcvb'.firstLetter() );
-console.log( 'VASDASDASDFDSFS'.firstLetter() );
-
-String.prototype.lastLetter = function () {
-    console.log( this );
-    return this[this.length-1];
-}
-
-console.log( 'afghfgxcvb'.lastLetter() );
-console.log( 'VASDASDASDFDSFS'.lastLetter() );
-
-*/
-
-
-
-/*
-Todo darba aprasancios savybes
-- description
-- deadline
-- status: todo, in-progress, done
-*/
-
-
-
-//console.log(todo_list);
-
 'use strict'
 
 function renderList( list ) {
@@ -168,6 +109,8 @@ function addnewTodoItem(){
     }
 
     todo_list.push( newTodo ) ; 
+
+    console.log(todo_list);
     
     return;  
 }
