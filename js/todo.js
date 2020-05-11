e58b680e76ebc057e1073ef23c33198643acf993
LinoGitas <63246130+LinoGitas@users.noreply.github.com>
2020-05-11 16:06:26 +0300

    add new

--- js/todo.js
+++ js/todo.js
@@ -124,3 +124,96 @@ function actionRemoveAll(){
     }
 }
 
+/******************************************
+Form Actions 
+********************************************/
+
+const DOMform = document.querySelector('.form');
+const DOMtaskTextArea = DOMform.querySelector('textarea[name="task"]');
+const DOMdeadlineInput = DOMform.querySelector('input[name="deadline"]');
+const DOMformActions = DOMform.querySelector('.actions');
+const DOMformAdd = DOMformActions.querySelector('.btn.add');
+const DOMformClear = DOMformActions.querySelector('.btn.clear');
+
+DOMdeadlineInput.value = formatDate( 86400000 ); //24*60*60*1000 //paros milisekundes 8640000;
+
+DOMformClear.addEventListener('click', clearForm);
+
+function clearForm(){
+    //console.log('clear');
+    //console.log(DOMtaskTextArea);
+    //console.log(DOMdeadlineInput);
+
+    DOMtaskTextArea.value = '';
+    DOMdeadlineInput.value = '';
+}
+
+DOMformAdd.addEventListener('click', addnewTodoItem);
+
+function addnewTodoItem(){
+
+    let newTodo = {
+        description: DOMtaskTextArea.value.trim(),
+        created_on: formatDate(),
+        deadline: DOMdeadlineInput.value.trim(),
+        status: 'todo'
+    };
+
+    if ( newTodo.description.length === 0){
+        console.error('ERROR: tuscias description');
+    }
+
+    if ( newTodo.deadline.length > 0 && (new Date(newTodo.deadline)).toString() === 'Invalid Date' ){
+        console.error('ERROR: nevalidus deadline');
+    }
+
+    todo_list.push( newTodo ) ; 
+    
+    return;  
+}
+
+function formatDate( deltaTime = 0 ){
+    let now = new Date();
+
+    if ( deltaTime !== 0 ){
+        now = new Date( Date.now() + deltaTime);
+    }
+
+    let minutes = now.getMinutes();
+    let hours = now.getHours();
+    let days = now.getDate();
+    let month = now.getMonth() + 1;
+    const years = now.getFullYear();
+
+    if ( minutes < 10 ){
+        minutes = '0'+minutes;
+    }
+    if ( hours < 10 ){
+        hours = '0'+hours;
+    }
+    if ( days < 10 ){
+        days = '0'+days;
+    }
+    if ( month < 10 ){
+        month = '0'+month;
+    }
+
+    return years+'-'+month+'-'+days+' '+hours+':'+minutes;
+}
+
+function addnewTodoItem0(){
+    
+    let newTodo = {};
+
+    console.log('TODO: istraukti "task" description');
+    console.log('TODO: istraukti "deadline"');
+
+    console.log('TODO: validavimas description');
+    console.log('TODO: validavimas deadline');
+
+    console.log('TODO: sukonstruojam "todo item" objekta');
+    console.log('TODO: statusas "todo"');
+    console.log('TODO: statusas tcreated_on NOW/DABAR');
+
+    return newTodo
+}
